dcbe47cc7a470d44c13168e94678fff203d928bf
Tanvi Singh <tanvi@liv-ex.com>
2020-10-26 11:58:53 +0530

    cache added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const mongoose = require("mongoose");
 const app = express();
 const ShortUrl = require('./models/shortUrls');
 
+
+const Memcached = require('memcached');
+const MEMCACHE_PORT = 11211;
+// time in seconds, default set to 30 mins
+const DEFAULT_CACHE_LIFETIME = 30 * 60;
+const memcached = new Memcached(`localhost:${MEMCACHE_PORT}`, { retries: 10, retry: 10000, remove: true });
+
+memcached.connect( 'localhost:11211', function( err, conn ){
+  if( err ) {
+  console.log( conn.server,'error while memcached connection!!');
+  }
+});
+
+const getValue = (key) => {
+  return new Promise((resolve, reject) => {
+    memcached.get(key, function (err, data) {
+      if (data) {
+        return resolve(data);
+      }
+      return resolve('');
+    });
+  });
+}
+
+ const setValue = (key, value, lifetime = DEFAULT_CACHE_LIFETIME) => {
+  return new Promise((resolve, reject) => {
+    memcached.set(key, value, lifetime, function (err) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(value);
+    });
+  });
+}
+
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -31,8 +66,16 @@ app.post('/shortUrls', async (req, res) => {
     let { url, customUrl } = req.body;
     const expiryTime = req.body.expiryTime;
     const shortUrls = await ShortUrl.find();
+
+    if (url) {
+      const urlRegex = new RegExp(/((https):\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+      let result = urlRegex.test(url);
+      if(!result) {
+        res.render('error',{msg:"incorrect url format"});
+      } 
+    }
   
-    console.log("custom name:", customUrl,url,expiryTime)
+    console.log("custom name:", customUrl,url,expiryTime);
   
     if(!expiryTime) {
       res.render('error',{msg:"provide expire time"});
@@ -40,31 +83,45 @@ app.post('/shortUrls', async (req, res) => {
     else if(expiryTime && Number.isNaN(expiryTime)){
       res.render('error',{msg:"provide valid expire time"});
     }
-  
-    let searched = await ShortUrl.findOne({url:url});
-    console.log('here----',searched)
-    if(searched) {
-      searched.searched++;
-      searched.save();
-      res.render('index',{ searched, shortUrls})
+
+    const urlCached = await getValue(url);
+    console.log('cache----',urlCached);
+    if(urlCached){
+      res.render('index',{ searched:urlCached, shortUrls});
     }
-    else {
-      const currentDate = new Date().getTime();
-      const expiryDate = new Date(currentDate + parseInt(expiryTime) * 1000);
-      if (customUrl == ''){
-        await ShortUrl.create({ url, customUrl, expiryDate })
+
+    if(!urlCached) {
+      //not found in cache then hit db
+      let searched = await ShortUrl.findOne({url:url});
+      if(searched) {
+        //searched 5 times so goes in cache
+        if(searched.searched >= 5) {           
+          setValue( url,searched ); //in ms
+        }
+        searched.searched++;
+        searched.save();
+        console.log('db----',searched);
+        res.render('index',{ searched, shortUrls})
       }
       else {
-        let nameExists = await ShortUrl.findOne({ customUrl });
-        if(nameExists){
-          res.render('error',{msg:"name already exists, choose another"});
+        const currentDate = new Date().getTime();
+        const expiryDate = new Date(currentDate + parseInt(expiryTime) * 1000);
+        if (customUrl == ''){
+          await ShortUrl.create({ url, customUrl, expiryDate })
         }
         else {
-          await ShortUrl.create({ url, urlCode:customUrl, expiryDate, customUrl })
+          let nameExists = await ShortUrl.findOne({ customUrl });
+          if(nameExists){
+            res.render('error',{msg:"name already exists, choose another"});
+          }
+          else {
+            await ShortUrl.create({ url, urlCode:customUrl, expiryDate, customUrl })
+          }
         }
       }
-    }
-    res.redirect('/')
+    }      
+
+    res.redirect('/');
   }
   catch(err) {
     res.render('error',{msg:err});
--- models/shortUrls.js
+++ models/shortUrls.js
@@ -31,7 +31,7 @@ const ShortUrlsSchema = new Schema({
     searched: {
         type: Number,
         required: true,
-        default: 0
+        default: 1
     }
 });
 
